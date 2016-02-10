e32ecfb6bef2aa87257c17b505f56cd54fe8be2e
Bruno Harger Alves <bhazero025@gmail.com>
2016-02-09 22:03:26 -0500

    Updated Deadzones and added tank drive
    Note: also added support for other motors and possible pneumatics.

--- src/team1649/Robot.java
+++ src/team1649/Robot.java
@@ -1,10 +1,12 @@
 package team1649;
 
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.RobotDrive.MotorType;
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
@@ -12,6 +14,9 @@ public class Robot extends SampleRobot
 {
     RobotDrive robotDrive;
     Joystick stick; //Controller
+    boolean isMecanum = true;
+    //VictorSP motorTest;
+    //Compressor numatics;
 
     // Channels for the wheels
     final int frontLeftChannel	= 0;
@@ -21,8 +26,6 @@ public class Robot extends SampleRobot
     
     // The channel on the driver station that the joystick is connected to
     final int joystickChannel	= 0;
-    private boolean[] deadZone = {false, false}; 
-    boolean whileDrive = true;
   
     
     public Robot() 
@@ -31,61 +34,80 @@ public class Robot extends SampleRobot
     	robotDrive.setInvertedMotor(MotorType.kFrontLeft, true);	// invert the left side motors
     	robotDrive.setInvertedMotor(MotorType.kRearLeft, true);		// you may need to change or remove this to match your robot
         robotDrive.setExpiration(0.1);
+        //motorTest = new VictorSP(5);
+        //numatics = new Compressor(); // Default is 0.
     
         stick = new Joystick(joystickChannel);
     }
+    
+    private void startPneumatics()
+    {
+    	//numatics.start();
+    	//numatics.setClosedLoopControl(true);
+    }
         
-
-    /**
-     * Runs the motors with Mecanum drive.
-     */
+    //We can now choose between tank drive and mecanum in a click.
     public void operatorControl() 
     {
         robotDrive.setSafetyEnabled(true);
         
         while (isOperatorControl() && isEnabled())
         {
-        	deadZone();
         	
-            /*
-        	for(int i = 0; i < deadZone.length; i++){
-        		if(deadZone[i])
-        		{
-        			whileDrive = false;
-        		}
-        	}*/
+        	if (stick.getRawButton(2))
+    		{
+    			isMecanum = false;
+    		}
             
-	       	if(whileDrive)
-	       	{
-	       		robotDrive.mecanumDrive_Cartesian(stick.getX(), stick.getY(), getZ(), 0);
-	        }
-	               
-	       	resetDeadzones();
-	        whileDrive = true;
+        	if (stick.getRawButton(1) || isMecanum)
+        	{
+        		isMecanum = true;
+        		robotDrive.mecanumDrive_Cartesian(stick.getX(), stick.getY(), getZ(), 0);
+        	}
+        	else
+        	{
+    	       	robotDrive.tankDrive(leftSpeed(), rightSpeed());
+        	}
+        	
+	           
+        	//motorTest.set(leftSpeed());
 	        
-	        SmartDashboard.putNumber("Joystick x", stick.getX());
-	        SmartDashboard.putNumber("Joystick y", stick.getY());
-	        SmartDashboard.putNumber("Joystick z", getZ());
-	        
-	        SmartDashboard.putBoolean("DeadZone 0", deadZone[0]);
-	        SmartDashboard.putBoolean("DeadZone 1", deadZone[1]);
+	        SmartDashboard.putNumber("leftSpeed(): ", leftSpeed());
+	        SmartDashboard.putNumber("rightSpeed(): ", rightSpeed());
+
+	        SmartDashboard.putBoolean("isMecanum: ", isMecanum);
 	       	
 	        Timer.delay(0.005);	// wait 5ms to avoid hogging CPU cycles
           
         }    
     }
     
-    private void deadZone()
+    private double leftSpeed()
+    {	
+    	double speed = stick.getRawAxis(1) * -1;
+    	
+    	if ((speed <= 0.25) & speed >=- 0.25)
+    	{
+    		return 0.0;
+    	}
+    	else
+    	{
+        	return speed;
+    	}
+    }
+    
+    private double rightSpeed()
     {
-    	if((stick.getX()<=0.25) && (stick.getX()>=-0.25))
+    	double speed = stick.getRawAxis(5);
+    	
+    	if ((speed <= 0.25) & speed >=- 0.25)
     	{
-    		deadZone[0] = true;
+    		return 0.0;
     	}
-    	if((stick.getY()<=0.25) && (stick.getY()>=-0.25))
+    	else
     	{
-    		deadZone[1]= true;
+        	return speed;
     	}
-
     }
 
     private double getZ()
@@ -98,19 +120,8 @@ public class Robot extends SampleRobot
     	{
         	return stick.getRawAxis(2);	
     	}
-    	
     	return 0.0;
     }
     
-    private void resetDeadzones()
-    {
-    	for(boolean dead : deadZone)
-    	{
-    		if(dead)
-    		{
-    			dead = false;
-    		}
-    	}
-    }
 }
 
